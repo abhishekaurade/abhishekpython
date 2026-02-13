7c3d5894a8614b22e35d88e94cdf4d06d6b812a8
abhishek <auradeabhishek06@gmail.com>
2026-02-13 16:03:10 +0530

    Add files via upload

---
+++ ABHISHEKEXP2.2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 15:53:46 2026
+
+@author: User
+"""
+n=int(input("enternumber."))
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+    print("factional:",fact)
