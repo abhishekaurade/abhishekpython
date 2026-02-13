87860b92f828155672c9f96755eaea0683cee974
abhishek <auradeabhishek06@gmail.com>
2026-02-13 15:29:03 +0530

    Add files via upload

---
+++ abhishek EXP1.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 15:05:57 2026
+
+@author: User
+"""
+name=input("enter your name:")
+age=input("enter your age:")
+student_class=input("enter your class:")
+division=input("enter your division:")
+roll_no=input("enter your roll no.:")
+batch=input("enter your batch:")
+phone_no=input("enter your phone no.:")
+blood_group=input("enter your blood group:")
+adress=input("enter your adress:")
+college=input("enter your college name:")
+city=input("enter your city name:")
+
+print("\n-----student information-------")
+
+
+
+
+
+print("name:",name)
+print("age:",age)
+print("student class:",student_class)
+print("division:",division)
+print("roll no:",roll_no)
+print("batch:",batch)
+print("phone no:",phone_no)
+print("blood group:",blood_group)
+print("adress:",adress)
+print("college:",college)
+print("city:",city)
