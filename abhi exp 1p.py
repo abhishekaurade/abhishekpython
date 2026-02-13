1e03bdef9214aa3622839b4ea99316945b1de00a
abhishek <auradeabhishek06@gmail.com>
2026-02-13 14:51:48 +0530

    Add files via upload

---
+++ abhi exp 1p.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 14:46:05 2026
+
+@author: User
+"""
+
+name = input("Enter your name: ") # String 
+age = int(input("Enter your age: ")) # Integer 
+height = float(input("Enter your height: ")) # Float
+# Printing the inputs 
+print("\nEntered Details:") 
+print("Name:", name)
+print("Age:", age) 
+print("Height:", height) 
